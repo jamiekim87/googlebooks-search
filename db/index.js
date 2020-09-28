b1dca854717e9b2853266a9ef0c6a129e5521553
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-28 11:24:39 -0700

    push code to repo

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/book_db', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_db', {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,25 +1,25 @@
 const router = require('express').Router()
 const { Book } = require('../models')
 
-router.get('/book', (req, res) => {
+router.get('api/books', (req, res) => {
   Book.find()
     .then(book => res.json(book))
     .catch(err => console.log(err))
 })
 
-router.post('/book', (req, res) => {
+router.post('/api/books', (req, res) => {
   Book.create(req.body)
     .then(book => res.json(book))
     .catch(err => console.log(err))
 })
 
-router.put('/book/:id', (req, res) => {
+router.put('/api/books/:id', (req, res) => {
   Book.findByIdAndUpdate(req.params.id, req.body)
     .then(book => res.json(book))
     .catch(err => console.log(err))
 })
 
-router.delete('/book/:id', (req, res) => {
+router.delete('/api/books/:id', (req, res) => {
   Book.findById(req.params.id)
     .then(book => book.remove())
     .then(book => res.json(book))
