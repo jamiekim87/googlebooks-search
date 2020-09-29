f507772a95dc46a329dc5b9cdcf6ea020310e171
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-29 12:34:13 -0700

    push code

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+  const {mediaState, setMediaState} = useState({
+    search: '',
+    media: []
+  })
+
+  mediaState.handleInputChange = event => {
+    setMediaState({ ...mediaState, [event.target.name]: event.target.value })
+  }
+
+  mediaState.handleSearchGoogle = event => {
+    event.preventDefault()
+  }
+
   return (
-    <h1>This is the home page.</h1>
+    <>
+    <h1>Search for Google Books</h1>
+    <form>
+      <p>
+        <label htmlFor="search">search</label>
+        <input 
+            type="text" 
+            name="search"
+            value={mediaState.search}
+            onChange={mediaState.handleInputChange} />
+      </p>
+      <p>
+        <button onClick={mediaState.handleSearchGoogle}>Search Google Books</button>
+      </p>
+    </form>
+    </>
   )
 }
 
