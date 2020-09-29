0942caa445dc50d191b9225533cf3c4cf27ed9d6
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-29 14:45:56 -0700

    git push

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,44 +1,9 @@
-import React, { useState } from 'react'
-import { getBook } from '../../utils/API'
+import React from 'react'
 
-const Home = () => {
-
-  const {bookState, setBookState} = useState({
-    search: '',
-    book: []
-  })
-
-  bookState.handleInputChange = event => {
-    setBookState({ ...bookState, [event.target.name]: event.target.value })
-  }
-
-  bookState.handleSearchGoogle = event => {
-    event.preventDefault()
-    getBook(bookState.search)
-        .then(({ data }) => {
-          console.log(data)
-        })
-        .catch(err => console.error(err))
-  }
-
+const Saved = () => {
   return (
-    <>
-    <h1>Search for Google Books</h1>
-    <form>
-      <p>
-        <label htmlFor="search">search</label>
-        <input 
-            type="text" 
-            name="search"
-            value={bookState.search}
-            onChange={bookState.handleInputChange} />
-      </p>
-      <p>
-        <button onClick={bookState.handleSearchGoogle}>Search Google Books</button>
-      </p>
-    </form>
-    </>
+    <h1>This is the home page.</h1>
   )
 }
 
-export default Home
+export default Saved
